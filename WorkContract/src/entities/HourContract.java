82e70b6e59e4abfd3d84494a8dbe4981a62358d8
WarlenSilvaa7 <warlensa007@gmail.com>
2024-02-25 16:18:18 -0300

    Adicionando os getters and setters da classe HourContract e os métodos em geral

--- WorkContract/src/entities/HourContract.java
+++ WorkContract/src/entities/HourContract.java
@@ -19,5 +19,35 @@ public class HourContract {
 	}
 	
 	
+	
+	public Date getDate() {
+		return date;
+	}
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
+
+	public Double getValuePerHour() {
+		return valuePerHour;
+	}
+
+	public void setValuePerHour(Double valuePerHour) {
+		this.valuePerHour = valuePerHour;
+	}
+
+	public Integer getHours() {
+		return hours;
+	}
+
+	public void setHours(Integer hours) {
+		this.hours = hours;
+	}
+
+	public double totalValue() {
+		return valuePerHour * hours;
+	}
+	
+	
 
 }
