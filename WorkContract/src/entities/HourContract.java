6789d1b3846bcc5929c870184e6d6afffae5a468
WarlenSilvaa7 <warlensa007@gmail.com>
2024-02-25 14:00:44 -0300

    Implementando as classes

--- WorkContract/src/entities/HourContract.java
+++ WorkContract/src/entities/HourContract.java
@@ -1,5 +1,23 @@
 package entities;
 
+import java.sql.Date;
+
 public class HourContract {
+	
+	private Date date;
+	private Double valuePerHour;
+	private Integer hours;
+	
+	public HourContract () {
+		
+	}
+
+	public HourContract(Date date, Double valuePerHour, Integer hours) {
+		this.date = date;
+		this.valuePerHour = valuePerHour;
+		this.hours = hours;
+	}
+	
+	
 
 }
